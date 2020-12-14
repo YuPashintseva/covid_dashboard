6f4b780a45efcb40ba86eb123c539ffd035a4d16
YuPashintseva <juliyapashintseva@gmail.com>
2020-12-14 12:05:03 +0600

    feat: add spots for all countries

--- src/api.js
+++ src/api.js
@@ -1,34 +1,10 @@
-// API SERVICES
+// API SERVICE
 
+// ANOTHER API https://disease.sh/v3/covid-19/countries?yesterday=true&twoDaysAgo=false&sort=cases&allowNull=true
 
-/*  INPUT PARAMS: Slug or ISO2
-    RETURN COUNTRIES SLUG OR ISO2 LIST DEPENING OF THE INPUT PARAMETER
-*/
-export async function getAllCountries(countryMode) {
-  let response = await fetch('https://api.covid19api.com/countries');
+export async function getTotalByAllCountries() {
+  let response = await fetch('https://disease.sh/v3/covid-19/countries?yesterday=true&twoDaysAgo=false&sort=cases&allowNull=true');
   let data = await response.json();
-  const countryList = data.map((item) => {
-    if (countryMode === 'Slug') {
-      return item.Slug
-    } else {
-      return item.ISO2;
-    }
-  });
-  return countryList;
+  return data;
 }
 
-// TOTAL CASES BY COUNTRY: CONFIRMED, RECOVERED, DEATHS, ACTIVE
-export async function getTotalByCountry(slug) {
-  console.log('country:'+slug)
-  let response = await fetch(`https://api.covid19api.com/live/country/south-africa/status/confirmed`);
-  let data = await response.json();
-  console.log('response:'+data)
-  const countriesSlug = data.map((item) => {
-    let arr = [];
-    arr.push(item.Confirmed);
-    arr.push(item.Lat);
-    arr.push(item.Lon);
-    return arr;
-  });
-  return countriesSlug;
-}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getAllCountries, getTotalByCountry } from './api';
+import { getAllCountries, getTotal, getTotalByAllCountries } from './api';
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 1.5);
 
@@ -11,34 +11,26 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoieXVwYXNoaW50c2V2YSIsImEiOiJja2lpZmY1eXAyNDJyMnFvNXM1M2F3YzF2In0.JPSKDwKVKULCE83pQoO4vg'
 }).addTo(mymap);
 
-//var marker = L.marker([51.5, -0.09]).addTo(mymap);
-/*var circle = L.circle([46.82, 8.23], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 5000
-}).addTo(mymap);
-*/
 
-//const countriesSlug  = getAllCountries('ISO2');
-let country='8';
-getAllCountries('Slug')
-  .then(data => {
-        country = data[0];
-        getTotalByCountry(country)
-            .then(data2 => {
-                makeCircle(data2.slice(-1)[0][1],data2.slice(-1)[0][2]);
-            })
-            
-    }); 
-//console.log(country);
 
-function makeCircle(lat, lon) {
+let totalCases = '';
+
+getTotalByAllCountries()
+    .then(data => {
+        totalCases = data; 
+        (totalCases).map((countryItem) => {
+            makeCircle(countryItem.countryInfo.lat, countryItem.countryInfo.long, countryItem.cases/50);
+            return countryItem;
+        })
+      }); 
+
+
+function makeCircle(lat, lon, rad) {
     var circle = L.circle([lat, lon], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 5000
+        radius: rad
     }).addTo(mymap);
 } 
 
