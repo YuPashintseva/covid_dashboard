acd5a0001e1262dddcc052f542791112d897fe77
Vagiz Musin <vagiz_mus@mail.ru>
2020-12-22 03:15:29 +0600

    feat: add new func n shorter code

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import {
   changeMapMode,
   NavigateToPoint,
 } from "./map";
-import  graph  from "./graphic.js";
+import graph from "./graphic.js";
 createMap();
 
 //
 const gr = new graph("allCases", "The World", "all");
-  gr.drawCheck("allCases", "The World", "all");
+gr.drawCheck("allCases", "The World", "all");
 
-  function onchange() {
-    gr.drawCheck("allCases", "The World", "all");
-  }
+function onchange() {
+  gr.drawCheck("allCases", "The World", "all");
+}
 //
 const CASESLIST = document.querySelector(".cases__list");
 const DEATHSLIST = document.querySelector(".deaths__list");
@@ -34,9 +34,45 @@ const CASESTABLEBODY = document.querySelector(".cases__table-body");
 
 const DATALISTOPTIONS = document.getElementById("datalistOptions");
 const FLAGIMG = document.querySelectorAll(".flag-img");
-//
-
-// CASES
+// OPTIONS OF DATE
+let options = {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+// CHANGE NUMBER, ADD SPACE BETWEEN NUMBERS
+function changeNumberAddSpace(n, symbol) {
+  return Number(n)
+    .toString()
+    .replace(/(?!null)(\d)(?=(\d\d\d)+([^\d]|$))/g, `$1${symbol}`);
+}
+// CREATE ARRAY OF LIST
+function convertToListItem(country, property) {
+  return `<a href="javascript:void(0)" class="list-group-item list-group-item-action deaths__item" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">${country.country}</h5>
+      <span class="cases__count badge badge-dark text-light fs-5 badge-pill">${changeNumberAddSpace(
+        country[property],
+        " "
+      )}</span>
+    </div>
+    <p class="mb-1"><img src="${country.countryInfo.flag}" alt="${
+    country.country
+  }" class="flag-img">
+    </p>
+    <div class="d-flex w-100 justify-content-between"> 
+     <small class="text-info wrap-normal">Population: ${changeNumberAddSpace(
+       country.population,
+       ","
+     )}</small>                        <small class="text-muted">${new Date(
+    country.updated
+  ).toLocaleString("en-US", options)}</small>
+                      </div>
+  </a>`;
+}
 
 async function getCasesDeathsRecoverd() {
   const url = `https://disease.sh/v3/covid-19/countries?yesterday=true&twoDaysAgo=false&sort=cases&allowNull=true`;
@@ -45,153 +81,56 @@ async function getCasesDeathsRecoverd() {
   let globalCases = 0;
   let globalDeaths = 0;
   let globalRecovered = 0;
-  let listCountry = ``;
-  let listDeaths = ``;
-  let listRecovered = ``;
   let listOptionValue = ``;
 
-  let options = {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  };
+  // CREATE CASES LIST
+  const casesArray = data
+    .sort((country1, country2) => {
+      return country1.cases < country2.cases ? 1 : -1;
+    })
+    .map((country) => {
+      return convertToListItem(country, "cases");
+    })
+    .join(" ");
+  // CREATE DEATHS LIST
+  const deathsArray = data
+    .sort((country1, country2) => {
+      return country1.deaths < country2.deaths ? 1 : -1;
+    })
+    .map((country) => {
+      return convertToListItem(country, "deaths");
+    })
+    .join(" ");
+  // CREATE RECOVERED LIST
+  const recoveredArray = data
+    .sort((country1, country2) => {
+      return country1.recovered < country2.recovered ? 1 : -1;
+    })
+    .map((country) => {
+      return convertToListItem(country, "recovered");
+    })
+    .join(" ");
+  //// INSERT IN HTML DOC
+  CASESLIST.insertAdjacentHTML("beforeend", casesArray);
+  DEATHSLIST.insertAdjacentHTML("beforeend", deathsArray);
+  RECOVEREDLIST.insertAdjacentHTML("beforeend", recoveredArray);
 
   for (let i = 0; i < data.length; i += 1) {
+    // CALCULATE ALL CASES IN EVERY GROUP
     globalCases += Number(data[i].cases);
     globalDeaths += Number(data[i].deaths);
     globalRecovered += Number(data[i].recovered);
 
-    listCountry += `<a href="javascript:void(0)" class="list-group-item list-group-item-action cases__item" aria-current="true">
-                      <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">${data[i].country}</h5>
-                        <span class="cases__count badge badge-warning text-dark fs-5 badge-pill">${Number(
-                          data[i].cases
-                        )
-                          .toString()
-                          .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")}</span>
-                      </div>
-                      <p class="mb-1"><img src="${
-                        data[i].countryInfo.flag
-                      }" alt="${data[i].country}" class="flag-img">
-                      </p>
-                      <div class="d-flex w-100 justify-content-between"> 
-                      <small>Population: ${Number(data[i].population)
-                        .toString()
-                        .replace(
-                          /(?!null)(\d)(?=(\d\d\d)+([^\d]|$))/g,
-                          "$1 "
-                        )}</small>                        <small class="text-muted">${new Date(
-      data[i].updated
-    ).toLocaleString("en-US", options)}</small>
-                      </div>
-                      
-
-                    </a>`;
-
-    // search
-    let mydata = new Date(data[i].updated);
-
-    //console.log(data[i].updated);
+    // CREATE TAGS OPTION IN DATALIST
     listOptionValue += `<option class="search__item" value="${data[i].country}">`;
-
-    // deaths
-    listDeaths += `<a href="javascript:void(0)" class="list-group-item list-group-item-action deaths__item" aria-current="true">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">${data[i].country}</h5>
-      <span class="cases__count badge badge-dark text-light fs-5 badge-pill">${Number(
-        data[i].deaths
-      )
-        .toString()
-        .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")}</span>
-    </div>
-    <p class="mb-1"><img src="${data[i].countryInfo.flag}" alt="${
-      data[i].country
-    }" class="flag-img">
-    </p>
-    <div class="d-flex w-100 justify-content-between"> 
-                      <small>Population: ${Number(data[i].population)
-                        .toString()
-                        .replace(
-                          /(?!null)(\d)(?=(\d\d\d)+([^\d]|$))/g,
-                          "$1 "
-                        )}</small>                        <small class="text-muted">${new Date(
-      data[i].updated
-    ).toLocaleString("en-US", options)}</small>
-                      </div>
-  </a>`;
-
-    // recovered
-
-    listRecovered += `<a href="javascript:void(0)" class="list-group-item list-group-item-action recovered__item" aria-current="true">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">${data[i].country}</h5>
-      <span class="cases__count badge badge-success text-light fs-5 badge-pill">${Number(
-        data[i].recovered
-      )
-        .toString()
-        .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")}</span>
-    </div>
-    <p class="mb-1"><img src="${data[i].countryInfo.flag}" alt="${
-      data[i].country
-    }" class="flag-img">
-    </p>
-    <div class="d-flex w-100 justify-content-between"> 
-                      <small>Population: ${Number(data[i].population)
-                        .toString()
-                        .replace(
-                          /(?!null)(\d)(?=(\d\d\d)+([^\d]|$))/g,
-                          "$1 "
-                        )}</small>                        <small class="text-muted text-end">${new Date(
-      data[i].updated
-    ).toLocaleString("en-US", options)}</small>
-                      </div>
-  </a>`;
   }
-
-  CASESLIST.insertAdjacentHTML("beforeend", listCountry);
-  DEATHSLIST.insertAdjacentHTML("beforeend", listDeaths);
-  RECOVEREDLIST.insertAdjacentHTML("beforeend", listRecovered);
-
-  // CASES
-  document.querySelectorAll(".cases__link").forEach((element, index, array) => {
-    element.addEventListener("click", function() {
-      if (index === 0) {
-        CASESTITLE.textContent = "Cases by Country";
-        CASESGLOBAL.textContent = data.Global.TotalConfirmed.toString().replace(
-          /(\d)(?=(\d\d\d)+([^\d]|$))/g,
-          "$1 "
-        );
-      } else if (index === 1) {
-        CASESTITLE.textContent = "Last Day Cases";
-        CASESGLOBAL.textContent = data.Global.NewConfirmed.toString().replace(
-          /(\d)(?=(\d\d\d)+([^\d]|$))/g,
-          "$1 "
-        );
-      } else if (index === 2) {
-        CASESTITLE.textContent = "Total 100K pop";
-        CASESGLOBAL.textContent = "формула";
-      } else if (index === 3) {
-        CASESTITLE.textContent = "Last Day 100K pop";
-        CASESGLOBAL.textContent = "формула";
-      }
-    });
-  });
 
   // DATA LIST OPTIONS
   DATALISTOPTIONS.insertAdjacentHTML("beforeend", listOptionValue);
 
-  CASESGLOBAL.textContent = globalCases
-    .toString()
-    .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
-  DEATHSGLOBAL.textContent = globalDeaths
-    .toString()
-    .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
-  RECOVEREDGLOBAL.textContent = globalRecovered
-    .toString()
-    .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
+  CASESGLOBAL.textContent = changeNumberAddSpace(globalCases, ",");
+  DEATHSGLOBAL.textContent = changeNumberAddSpace(globalDeaths, ",");
+  RECOVEREDGLOBAL.textContent = changeNumberAddSpace(globalRecovered, ",");
 
   // CASES ITEM CLICK
   document.querySelectorAll(".cases__item").forEach((element) => {
@@ -221,57 +160,61 @@ async function getCasesDeathsRecoverd() {
   });
 }
 //mode switchers
-document.querySelector('#nav-home-tab').addEventListener("click", function() {
-  changeMapMode('total_cases');
-  
+document.querySelector("#nav-home-tab").addEventListener("click", function() {
+  changeMapMode("total_cases");
+
   gr.drawCheck("allCases", gr.country, gr.proportion);
 });
 
-document.querySelector('#nav-profile-tab').addEventListener("click", function() {
-  changeMapMode('fatal_cases');
-  gr.drawCheck("deaths", gr.country, gr.proportion);
-});
+document
+  .querySelector("#nav-profile-tab")
+  .addEventListener("click", function() {
+    changeMapMode("fatal_cases");
+    gr.drawCheck("deaths", gr.country, gr.proportion);
+  });
 
-document.querySelector('#nav-contact-tab').addEventListener("click", function() {
-  changeMapMode('recover_cases');
-  gr.drawCheck("recovered", gr.country, gr.proportion);
-});
+document
+  .querySelector("#nav-contact-tab")
+  .addEventListener("click", function() {
+    changeMapMode("recover_cases");
+    gr.drawCheck("recovered", gr.country, gr.proportion);
+  });
 
 // FULLSCREEN
-let flagFull = true;
-document.querySelectorAll(".fullscreen").forEach((element) => {
-  element.addEventListener("click", function() {
-    // map
-    if (
-      flagFull &&
-      this.parentNode.classList.contains("col-6") &&
-      this.parentNode.classList.contains("col-sm-6") &&
-      this.parentNode.classList.contains("col-md-6") &&
-      this.parentNode.classList.contains("col-lg-6") &&
-      this.parentNode.classList.contains("col-xl-6")
-    ) {
-      flagFull = false;
-      this.parentNode.classList.remove("col-6");
-      this.parentNode.classList.remove("col-sm-6");
-      this.parentNode.classList.remove("col-md-6");
-      this.parentNode.classList.remove("col-lg-6");
-      this.parentNode.classList.remove("col-xl-6");
-      this.parentNode.classList.add("full");
-    } else {
-      flagFull = true;
-      this.parentNode.classList.add("col-6");
-      this.parentNode.classList.add("col-sm-6");
-      this.parentNode.classList.add("col-md-6");
-      this.parentNode.classList.add("col-lg-6");
-      this.parentNode.classList.add("col-xl-6");
-      this.parentNode.classList.remove("full");
-    }
-    // Cases by Country
-    // Global Deaths
-    // Recovered
-    // Graphic
-  });
-});
+// let flagFull = true;
+// document.querySelectorAll(".fullscreen").forEach((element) => {
+//   element.addEventListener("click", function() {
+//     // map
+//     if (
+//       flagFull &&
+//       this.parentNode.classList.contains("col-6") &&
+//       this.parentNode.classList.contains("col-sm-6") &&
+//       this.parentNode.classList.contains("col-md-6") &&
+//       this.parentNode.classList.contains("col-lg-6") &&
+//       this.parentNode.classList.contains("col-xl-6")
+//     ) {
+//       flagFull = false;
+//       this.parentNode.classList.remove("col-6");
+//       this.parentNode.classList.remove("col-sm-6");
+//       this.parentNode.classList.remove("col-md-6");
+//       this.parentNode.classList.remove("col-lg-6");
+//       this.parentNode.classList.remove("col-xl-6");
+//       this.parentNode.classList.add("full");
+//     } else {
+//       flagFull = true;
+//       this.parentNode.classList.add("col-6");
+//       this.parentNode.classList.add("col-sm-6");
+//       this.parentNode.classList.add("col-md-6");
+//       this.parentNode.classList.add("col-lg-6");
+//       this.parentNode.classList.add("col-xl-6");
+//       this.parentNode.classList.remove("full");
+//     }
+//     // Cases by Country
+//     // Global Deaths
+//     // Recovered
+//     // Graphic
+//   });
+// });
 
 getCasesDeathsRecoverd();
 
